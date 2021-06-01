63b6c1a598d1b7a3e0a555077034d356a6f6fe60
Tomoya Otake <tomoya.ton@gmail.com>
2021-06-01 10:43:18 +0900

    Update docs to change the find function

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 
 - consult-ghq
 
-Find repository files using consult-find
+Find repository files using [affe-find](https://github.com/minad/affe), similar to consult-find.
+
+If you want to use consult-find instead, you can change like bellow:
+
+```elisp
+(setq consult-ghq-find-function #'consult-find)
+```
--- consult-ghq.el
+++ consult-ghq.el
@@ -26,7 +26,11 @@
 
 ;; This packaage provides qhq interface using Consult.
 ;;
-;; Its main entry points are the commands `consult-ghq'.
+;; Its main entry points are the commands `consult-ghq'.  Default
+;; find-function is affe-find.  If you want to use consult-find
+;; instead, you can change like bellow:
+;;
+;; (setq consult-ghq-find-function #'consult-find)
 
 ;;; Code:
 
