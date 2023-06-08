4913477c52c4e8748d0b8489ccfa6ea5ccb08221
Alan Pearce <alan@alanpearce.eu>
2023-06-08 07:55:28 +0200

    Fix typos

--- consult-ghq.el
+++ consult-ghq.el
@@ -24,11 +24,11 @@
 
 ;;; Commentary:
 
-;; This packaage provides qhq interface using Consult.
+;; This packaage provides ghq interface using Consult.
 ;;
 ;; Its main entry points are the commands `consult-ghq-find' and
 ;; `consult-ghq-grep`.  Default find-function is affe-find.  If you
-;; want to use consult-find instead, you can change like bellow:
+;; want to use consult-find instead, you can change like below:
 ;;
 ;; (setq consult-ghq-find-function #'consult-find)
 
